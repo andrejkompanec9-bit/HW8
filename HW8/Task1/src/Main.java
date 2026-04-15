c8182786a2c56c5738886ff318f4d6e7d8ae0021
andrejkompanec9-bit <andrejkompanec9@gmail.com>
2026-04-15 20:09:34 +0300

    Массив 10

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
 
-        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("=================");
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
-
+for (int i = array.length - 1; i >= 0; i-- ){
+    System.out.println(array[i]);
+}
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        System.out.println("=================");
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array [i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+        }
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
     }
 }
